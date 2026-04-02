8e975bb8390e8c8ddddcb46ca8ef694d4bbf40ee
taaziz1 <taaziz.b@gmail.com>
2026-04-01 19:56:58 -0700

    implement basic file io

--- src/MiniMars.java
+++ src/MiniMars.java
@@ -1,3 +1,22 @@
-void main() {
-    IO.println("Hello, world!");
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class MiniMars {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            if (Files.exists(Paths.get(arg))) {
+                System.out.println(arg);
+                File f = new File("output/" + arg + ".asm");
+                try {
+                    if(f.createNewFile()) {
+                        System.out.println("Created: " + arg + ".asm");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
