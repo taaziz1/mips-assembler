eefdd7062036d552000d5ba8abae5fa2d8ea1b10
taaziz1 <taaziz.b@gmail.com>
2026-04-02 00:22:59 -0700

    implement lexer to generate tokens

--- src/MiniMars.java
+++ src/MiniMars.java
@@ -1,22 +1,65 @@
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class MiniMars {
     public static void main(String[] args) {
+        HashSet<String> r_type = new HashSet<String>(
+                Arrays.asList("AND", "OR", "ADD", "SUB", "SLT", "MUL", "DIV", "NOR"
+                ));
+        HashSet<String> i_type = new HashSet<String>(
+                Arrays.asList("ADDI", "LB", "SB", "BNE", "BEQ")
+        );
+        HashSet<String> pseudo = new HashSet<String>(
+                Arrays.asList("NOP", "MOVE", "SLL", "LA", "LI", "ADDI", "STOP")
+        );
+
         for (String arg : args) {
-            if (Files.exists(Paths.get(arg))) {
-                System.out.println(arg);
-                File f = new File("output/" + arg + ".asm");
-                try {
-                    if(f.createNewFile()) {
-                        System.out.println("Created: " + arg + ".asm");
+            ArrayList<ArrayList<String>> tokenized_lines = new ArrayList<>();
+
+            // Lexer: converts lines to tokens
+            try (BufferedReader br = new BufferedReader(new FileReader(arg))) {
+                String line;
+
+                lineLoop:
+                while ((line = br.readLine()) != null) {
+                    ArrayList<String> tokens = new ArrayList<>();
+                    StringBuilder token = new StringBuilder();
+
+                    for (int i = 0; i < line.length(); i++) {
+                        char c = line.charAt(i);
+
+                        // If a comment is encountered, jump to the next line
+                        if (c == '#') {
+                            if (!token.isEmpty()) tokens.add(token.toString());
+                            continue lineLoop;
+                        }
+
+                        // If an unwanted character is encountered (whitespace, parentheses, or commas), skip it
+                        if (Character.isWhitespace(c) || c == ',' || c == '(' || c == ')') {
+                            if (!token.isEmpty()) {
+                                tokens.add(token.toString());
+                                token = new StringBuilder();
+                            }
+                        } else {
+                            token.append(c);
+                        }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    if (!token.isEmpty()) tokens.add(token.toString());
+                    if (!tokens.isEmpty()) tokenized_lines.add(tokens);
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+//            for(ArrayList<String> tokens : tokenized_lines) {
+//                System.out.print("[");
+//                for(String token : tokens) {
+//                    System.out.print(token + ", ");
+//                }
+//                System.out.println("] " + tokens.size());
+//            }
         }
     }
 }
